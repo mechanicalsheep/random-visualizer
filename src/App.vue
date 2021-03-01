3b5fdc52500e66dd7fcc8de12dd47fbaec1b6ef0
mechanicalsheep <shadyakhalil@gmail.com>
2021-03-01 12:10:10 +0300

    Initial Construction

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,57 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="yellow"
+      dark
+      height="100%"
+    >
+  
+    <v-row style="padding-top:10px;">
+      <v-col :cols="12">
+    <h2 style="color: black; text-align:center;">Random Visualizer</h2>
+      </v-col>
+    
+    
+      <v-col cols="6">
+        <v-text-field v-model="numNodes" light color="black" label="Number of nodes"/>
+      </v-col>
+    
+    <v-col cols="6">
+        <v-text-field v-model="randomization" light color="black" label="Randomization"/>
+      </v-col>
+    </v-row>
+ 
+    </v-app-bar>
+
+    <v-main>
+      <v-container>
+        <div style="max-width:fit-content" v-for="index in numNodes" :key="index">
+         
+      <Node/>
+        </div>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Node from './components/Node'
 
 export default {
   name: 'App',
+
   components: {
-    HelloWorld
-  }
-}
+    Node
+  },
+
+  data: () => ({
+    numNodes:5,
+    randomization:''
+  }),
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/Node.vue
@@ -0,0 +1,44 @@
+<template>
+    <v-container style="max-width:fit-content;">
+        <v-card width="80px" height="100px" color="yellow">
+            <v-card-title class="justify-center">
+                <div class="status "></div>
+            </v-card-title>
+            <v-card-text class="text-center">
+                <h3 style="font-weight:bolder;">{{index}}</h3>
+
+                </v-card-text>
+                <v-card-actions class="justify-center">
+                    <h2>{{counter}}</h2>
+                </v-card-actions>
+        </v-card>
+    </v-container>
+</template>
+
+<script>
+export default {
+    name:"Node",
+    data(){
+        return{
+            isActive:false,
+            index:1,
+            counter:15
+        }
+    }
+}
+</script>
+
+<style scoped>
+.status{
+    border-radius: 50%;
+width: 25px;
+height:25px;
+border: 1px solid black;
+background-color:lightgray;
+text-align: center;
+/* fill: green; */
+}
+.active {
+    background-color: green;
+}
+</style>
